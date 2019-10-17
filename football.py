d873f693aaace9e1040cdad74e08ca2183f4082a
Arthur Bowers <arthur.bowers@cgi.com>
2019-10-17 10:14:30 +0100

    Added some 'magic' commands (see streamlit documentation).

--- football.py
+++ football.py
@@ -3,10 +3,18 @@ import pandas as pd
 import numpy as np
 import plotly_express as px
 
-df = pd.read_csv('football_data.csv', encoding='ISO-8859-1')
+'''
+# Club and Nationality App
 
-clubs = st.multiselect('Show Player for Clubs?', df['Club'].unique())
-nationalities = st.multiselect(
+This very simple webapp allows you to select and visualize players from certain clubs and certain nationalities.
+'''
+
+# works without encoding? maybe a windows thing?
+df = st.cache(pd.read_csv)('football_data.csv')
+## df = pd.read_csv('football_data.csv', encoding='ISO-8859-1')
+
+clubs = st.sidebar.multiselect('Show Player for Clubs?', df['Club'].unique())
+nationalities = st.sidebar.multiselect(
     'Show Player from Nationalities?', df['Nationality'].unique())
 
 # Filter dataframe
@@ -15,8 +23,12 @@ new_df = df[(df['Club'].isin(clubs)) & (df['Nationality'].isin(nationalities))]
 # Write dataframe to screen
 st.write(new_df)
 
-# Create figure using plotly express
+# Create distplot with custom bin_size
 fig = px.scatter(new_df, x='Overall', y='Age', color='Name')
+
+'''
+### Here is a simple chart between player age and overall.
+'''
 
 # Plot
 st.plotly_chart(fig)
