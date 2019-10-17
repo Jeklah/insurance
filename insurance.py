64c25d16dccb50d42e81421708b905b10be929d5
Arthur Bowers <arthur.bowers@cgi.com>
2019-10-17 14:20:04 +0100

    Completed basics of insurance.py. Filtered results now search for one or more of the filterable results.

---
+++ insurance.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly_express as px
+
+'''
+# An Insurance Statistics WebApp
+'''
+# Read data from csv file
+df = st.cache(pd.read_csv)('Auto_Insurance_Claims_Sample.csv')
+
+# Setting up the sidebar menu
+educationLvl = st.sidebar.multiselect(
+    'Show clients with education level of:', df['Education'].unique())
+if educationLvl == "":
+    educationLvl = "*"
+states = st.sidebar.multiselect(
+    'Show clients living in state:', df['State'].unique())
+coverageLvl = st.sidebar.multiselect(
+    'Show clients with coverage level:', df['Coverage'].unique())
+employeed = st.sidebar.multiselect(
+    'Employment status?', df['EmploymentStatus'].unique())
+st.sidebar.markdown('Do you want to look up Male or Female clients?')
+if st.sidebar.checkbox('Male'):
+    df['Gender'].isin(gendMaleChkBx)
+if st.sidebar.checkbox('Female'):
+    df['Gender'].isin(gendFemChkBx)
+
+# Filter data_frame
+filteredDf = df[(df['Education'].isin(educationLvl)) |
+                (df['State'].isin(states)) |
+                (df['Coverage'].isin(coverageLvl)) |
+                (df['EmploymentStatus'].isin(employeed))]
+
+if st.checkbox('Do you want to see the data?'):
+    st.write(df)
+
+if st.checkbox('Do you want to see the filtered data?'):
+    st.write(filteredDf)
