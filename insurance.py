d226c8b014a9e73300aad2858dd610458be3221d
Arthur Bowers <arthur.bowers@cgi.com>
2019-10-18 14:52:37 +0100

    Got a scatter graph working which is interactive and draws a histogram based on what points are selected.

--- insurance.py
+++ insurance.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly_express as px
+import altair as alt
 
 '''
 # An Insurance Statistics WebApp
@@ -20,7 +21,6 @@ coverageLvl = st.sidebar.multiselect(
 employed = st.sidebar.multiselect(
     'Employment status?', df['EmploymentStatus'].unique())
 st.sidebar.markdown('Do you want to look up Male or Female clients?')
-
 
 # Filter data_frame
 if len(educationLvl) == 0:
@@ -47,3 +47,22 @@ if st.checkbox('Do you want to see the data?'):
 
 if st.checkbox('Do you want to see the filtered data?'):
     st.write(filteredDf)
+
+if st.checkbox('Would you like to see a comparison between total claim amount and months since last claim?'):
+    brush = alt.selection(type='interval')
+    points = alt.Chart(df).mark_point().encode(
+        x='Months Since Last Claim',
+        y='Total Claim Amount',
+        color=alt.condition(brush, 'Total Claim Amount',
+                            alt.value('lightgrey'))
+    ).add_selection(
+        brush
+    )
+    bars = alt.Chart(df).mark_bar().encode(
+        y='Months Since Last Claim:N',
+        color='Months Since Last Claim:N',
+        x='count(Total Claim Amount):Q'
+    ).transform_filter(
+        brush
+    )
+    points & bars
